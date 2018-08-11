42edd9ce6476054b205318c140f0448aab682062
KonstantinGrigorov <K.Grigorov@i.ua>
2018-08-11 12:08:58 +0300

    Observer one more not working

---
+++ app/code/local/Magicstore/Lesson/Model/Block.php
@@ -0,0 +1,11 @@
+<?php
+
+class Magicstore_Lesson_Model_Block extends Mage_Cms_Model_Block
+{
+    /**
+     * Prefix of model events names
+     *
+     * @var string
+     */
+    protected $_eventPrefix = 'my_cms_block';
+}
